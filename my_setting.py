79edf2bf18c886c21810b3dffe77ef764a617a47
yu <yu@yuui-MacBookPro.local>
2021-05-25 15:00:26 +0900

    프로젝트 환경설정 수정

--- my_setting.py
+++ my_setting.py
@@ -3,7 +3,7 @@ SECRET_KEY = 'django-insecure-_xvwqb87c@s2xpls70!l6n9g3r6q#rcgmcr)h=ibt&jvx*9075
 DATABASES = {
     'default' : {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'westarbucks',
+        'NAME': 'ownersdog',
         'USER': 'root',
         'PASSWORD': 'dnflskfk',
         'HOST': '127.0.0.1',
